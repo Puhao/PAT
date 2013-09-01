510d34320119fb6269e58ee38af34687c010efee
puhao <yuanpuhao@gmail.com>
2013-09-01 12:22:21 +0800

    oj测试通过

--- PAT(A)101-105-2-2013-08-30/dating.cpp
+++ PAT(A)101-105-2-2013-08-30/dating.cpp
@@ -8,52 +8,58 @@ using namespace std;
 
 void dating(string a,string b,string c,string d)
 {
-	char FstLet,SecLet;
-	int count = 0;
+	int day = -1;
+	int hour = -1;
+	int min = -1;
 	for(int i=0;i<a.size();i++)
 	{
-		if ((a[i] == b[i]) && (isupper(a[i])))
+		if (a[i] == b[i]) 
 		{
-			if (count == 0)
+			if (day == -1)
 			{
-				count = 1;
-				FstLet = a[i];
+				if(a[i] >= 'A' && a[i] <= 'G')
+				{
+					day = a[i] - 'A';
+				}
 			}
-			if (count == 1)
+			else if (hour == -1)
 			{
-				count = 2;
-				SecLet = a[i];
-			}
+				if(a[i] >= 'A' && a[i] <= 'N')
+				{
+					hour = a[i] - 'A' + 10;
+					break;
+ 				}
+ 				else if (isdigit(a[i]))
+ 				{
+ 					hour = a[i] - '0';
+ 					break;
+ 				}
+ 			}
 		}
 	}
+
 	string Week[] = {"MON", "TUE", "WED", "THU", "FRI","SAT","SUN"};
-	int hour,minu;
-	if (isdigit(SecLet))
-		hour = SecLet - '0';
-	else
-		hour = SecLet - 'A' + 10;
-	count = 0;
 	for(int i=0;i<c.size();i++)
 	{
 		if ( (c[i] == d[i]) && (isalpha(c[i])) )
 		{
-			if (count == 0)
-			{
-				count = 1;
-				minu = i;
-			}
+			min = i;
+			break;
 		}
 
 	}
 	cout.fill('0');
-	cout << Week[FstLet - 'A'] << " " << setw(2) << hour << ":" << setw(2) << minu << endl;
+	cout << Week[day] << " " << setw(2) << hour << ":" << setw(2) << min << endl;
 	return;
 
 }
 
 int main()
 {
-	string StrAray[] = {"3485djDkxh4hhGE", "2984akDfkkkkggEdsb", "s&hgsfdk", "d&Hyscvnm"};
+	//string StrAray[] = {"3485djDkxh4hhGE", "2984akDfkkkkggEdsb", "s&hgsfdk", "d&Hyscvnm"};
+	string StrAray[4];
+	for(int i=0;i<4;i++)
+		cin >> StrAray[i];
 	dating(StrAray[0],StrAray[1],StrAray[2],StrAray[3]);
 	return 0;
 }
